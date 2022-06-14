576c90fee596ed995f46a2fd4dba4fd3ab984c8b
PesAdam <pesadam15@gmail.com>
2022-06-14 09:15:30 +0200

    post

--- api/serializer.py
+++ api/serializer.py
@@ -5,3 +5,8 @@ class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'vote_to_skip', 'created_at')
+
+class CreateRoomSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,status
 from .models import Room
-from .serializer import RoomSerializer
+from .serializer import RoomSerializer, CreateRoomSerialize
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
 
@@ -9,3 +12,27 @@ class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerialize
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+            queryset = Room.objects.filter(host=host)
+
+            if queryset.exists():
+                room = queryset[0]
+                room.quest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            else:
+                room = Room(host=host, guest_can_pause = guest_can_pause, votes_to_skip = votes_to_skip)
+                room.save()
+            
+            return Response(RoomSerializer(room).data, status = status.HTTP_201_CREATED) 
